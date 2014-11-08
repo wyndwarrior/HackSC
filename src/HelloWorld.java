d17665157775338152665ebf3b6ca758eff8ad35
YuXuan Liu <wynd07@gmail.com>
2014-11-08 17:32:50 +0000

    Added data collection

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,19 +3,30 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class HelloWorld extends HttpServlet {
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    
+    String s = "";
+    public void service(HttpServletRequest request, HttpServletResponse response)
 	throws IOException, ServletException
     {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Hello World!</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>Hello World!</h1>");
-        out.println("</body>");
-        out.println("</html>");
+	response.setContentType("text/html");
+	String action = request.getParameter("action");
+	if( action != null ){
+	    PrintWriter out = response.getWriter();
+	    switch(action){
+	    case "put":
+		s += request.getParameter("data");
+		out.println("success");
+		break;
+	    case "get":
+		out.println(s);
+		break;
+	    case "clear":
+		s = "";
+		break;
+	    }
+	}else{
+	    PrintWriter out = response.getWriter();
+	    out.println("Malformed request");
+	}
     }
 }
