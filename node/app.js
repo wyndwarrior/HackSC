bc31a993baa8fe88bfdb97f401a94c5e594f1b93
Ian Macalinao <ian@giza.us>
2014-11-08 12:45:10 -0600

    please

--- node/app.js
+++ node/app.js
@@ -2,7 +2,9 @@ var express = require('express');
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
-app.use(require('body-parser').text());
+app.use(require('body-parser').urlencoded({
+  extended: true
+}));
 
 var db = {};
 
