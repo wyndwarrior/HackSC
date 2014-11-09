29681590e1475fe761cc4111bca28e0eec365477
Ian Macalinao <ian@giza.us>
2014-11-09 00:08:38 -0600

    Allow patient creation

--- node/public/js/client.js
+++ node/public/js/client.js
@@ -1,8 +1,46 @@
-angular.module('hacksc', ['restangular'])
+angular.module('hacksc', ['restangular', 'ui.bootstrap'])
 
-.controller('MainCtrl', function($scope) {
+.controller('MainCtrl', function($scope, Restangular, $modal) {
 
   $scope.title = 'MapReduceMyFitness';
 
+  var Patients = Restangular.all('api/patients');
+  var Prescriptions = Restangular.all('api/prescriptions');
+  var Foods = Restangular.all('api/foods');
+
+  $scope.patients = Patients.getList().$object;
+  $scope.prescriptions = Prescriptions.getList().$object;
+  $scope.foods = Foods.getList().$object;
+
+  $scope.openNewPatientModal = function() {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'tpls/add-patient-modal.html',
+      controller: function($scope, $modalInstance) {
+        $scope.create = function() {
+          $modalInstance.close($scope.patientName);
+        };
+
+        $scope.cancel = function() {
+          $modalInstance.dismiss(null);
+        };
+      },
+      size: 'sm'
+    });
+
+    modalInstance.result.then(function(patientName) {
+      if (!patientName) {
+        return;
+      }
+
+      console.log(patientName);
+      
+      Patients.post({
+        name: patientName
+      }).then(function() {
+        $scope.patients = Patients.getList().$object;
+      });
+    });
+  };
 
 });
