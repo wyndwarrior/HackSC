3e83fdb756b09e9d58d765b51ccb3fb074ecb9f2
YuXuan Liu <wynd07@gmail.com>
2014-11-09 05:16:27 +0000

    Update canvas

--- node/public/js/main.js
+++ node/public/js/main.js
@@ -24,23 +24,38 @@ function api(cmd, data, callb){
 }
 
 function startPull(){
-    setInterval(pull, 250);
+    setInterval(pull, 70);
 }
+
+var canvases = 0;
 
 function pull(){
     api("positions", null, function(data){
+	var cap = $('#rCapture');
 	if( data ){
-	    var cap = $('#rCapture');
-	    cap.html('');
+	    while(canvases < data.length){
+		cap.append('<div id="cap_'+canvases+'" class="col-sm-12"><canvas width="500" height="400" id="can'+canvases+'"></canvas><br/><a class="btn btn-success" onClick="chooseCanvas('+canvases+')" id="cap'+canvases+'" data-loading-text="Capturing in 3...">Capture Pose</a><br/></div>');
+		canvases++;
+	    }
+	    while(canvases > data.length){
+		$("#cap_"+(canvases-1)).remove();
+		canvases--;
+	    }
+		
 	    for(var i in data){
-		var x = data[i];
-		cap.append('<div class="col-sm-12"><canvas id="can'+i+'"></canvas><br/><a class="btn btn-success" onClick="chooseCanvas('+i+')">Capture Pose</a><br/></div>');
-		$("#can"+i).width(500);
-		$("#can"+i).height(400);
-		drawIn(x, i);
+		drawIn(data[i], i);
 	    }
 	}
     });
+}
+
+function chooseCanvas(can){
+    $("#cap"+can).button('loading');
+    setTimeout(function(){snapshot(can);}, 3000);
+}
+
+function snapshot(can){
+    $("#cap"+can).button('reset');
 }
 
 var order = "SpineBase SpineMid SpineMid SpineShoulder SpineShoulder Neck Neck Head SpineShoulder ShoulderLeft ShoulderLeft ElbowLeft ElbowLeft WristLeft WristLeft HandLeft SpineShoulder ShoulderRight ShoulderRight ElbowRight ElbowRight WristRight WristRight HandRight SpineBase HipLeft HipLeft KneeLeft KneeLeft AnkleLeft AnkleLeft FootLeft SpineBase HipRight HipRight KneeRight KneeRight AnkleRight AnkleRight FootRight HandTipLeft HandLeft HandLeft ThumbLeft HandTipRight HandRight HandRight ThumbRight".split(" ");
@@ -51,22 +66,38 @@ function drawLine(p1, p2, context){
     context.moveTo(p1[0], p1[1]);
     context.lineTo(p2[0], p2[1]);
     //console.log(p1[0], p1[1], p2[0], p2[1]);
-    context.lineWidth = 2;
+    context.lineWidth = 5;
     context.lineCap = 'round';
     context.strokeStyle = 'red';
     context.stroke();
+    point(p1[0], p1[1], context);
+        point(p2[0], p2[1], context);
+}
 
+function point(x, y, context){
+  context.beginPath();
+  context.moveTo(x, y);
+  context.lineTo(x+1, y+1);
+context.lineWidth = 7;
+    context.lineCap = 'round';
+    context.strokeStyle = 'blue';
+  context.stroke();
 }
 
 var LEN = 1;
 
 function to2D(p, w, h){
-    return [w*(p[0]+LEN)/(2*LEN), h*(1-(p[1]+LEN)/(2*LEN))]
+    return [Math.floor(w*(p[0]+LEN)/(2*LEN)), Math.floor(h*(1-(p[1]+LEN)/(2*LEN)))]
 }
 
 function drawIn(data, i){
     canvas = document.getElementById('can'+i);
     context = canvas.getContext('2d');
+
+    context.save();
+    context.setTransform(1, 0, 0, 1, 0, 0);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.restore();
     for(var i = 0; i<order.length; i+=2){
 	drawLine(to2D(data[order[i]], canvas.width, canvas.height), to2D(data[order[i+1]], canvas.width, canvas.height), context);
     }
